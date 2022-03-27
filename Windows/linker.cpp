a00c31a88fba458486becefe6c2671c8eff42f55
Kelvin <developer_lby@outlook.com>
2022-03-27 18:50:28 +0800

    2022-3-27

--- Windows/linker.cpp
+++ Windows/linker.cpp
@@ -1,35 +1,36 @@
-﻿/* ********************************************************************************
- *                        Copyright (C) 2022 Kelvin_LBY
+﻿/* **********************************************************************************
+ *                                  linker 
+ *                       Copyright (C) 2022 Kelvin_LBY
  *
- *     This program is free software: you can redistribute it and/or modify
- *     it under the terms of the GNU General Public License as published by
- *     the Free Software Foundation, either version 3 of the License, or
- *     (at your option) any later version.
+ *     This program is free software: you can redistribute it and/or modify it under
+ *     the terms of the GNU General Public License as published bythe Free Software
+ *     Foundation, either version 3 of the License, or any later version.
  *
- *     This program is distributed in the hope that it will be useful,
- *     but WITHOUT ANY WARRANTY; without even the implied warranty of
- *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *     GNU General Public License for more details.
+ *     This program is distributed in the hope that it will be useful, but WITHOUT
+ *     ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ *     FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
  *
  *     You should have received a copy of the GNU General Public License
- *     along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *     along with this program. If not, see <https://www.gnu.org/licenses/>.
  * 
  *     License: https://github.com/Kelvinlby/linker/LICENSE.md
  * 
- * ********************************************************************************/
+ * **********************************************************************************/
 
 // --- includes
 #include <iostream>
 #include <thread>
 #include <windows.h>
 #include <winsock.h>
+#include <csignal>
 #include "./inc/color.h"
  
 #pragma comment(lib,"wsock32.lib")
 #pragma comment (lib,"ws2_32.lib")
 
-// --- defination
+// --- defination and vars
 #define version "1.0.0"
+static volatile int keepRunning = 1;
 
 // --- usings
 using namespace std;
@@ -38,6 +39,17 @@ using namespace std;
 void about();
 int server( const char* ip );
 int client( const char* ip );
+void Send( SOCKET client, char send_buf[] );
+void Recv( SOCKET client, char send_buf[] );
+
+// --- set keyboard interrupt
+void sig_handler(int sig)
+{
+    if( sig == SIGINT )
+    {
+        keepRunning = 0;
+    }
+}
 
 // --- functions
 int main( int argv, char* args[] )
@@ -71,17 +83,18 @@ int main( int argv, char* args[] )
 
 int server( const char* ip )
 {
+    int len;
+    char send_buf[100];
+    char recv_buf[100];
+
+    thread thread_send, thread_recv;
     WSADATA wsa_data;
+    SOCKET server, client;
+    SOCKADDR_IN server_addr, client_addr;
+
     WSAStartup( MAKEWORD( 2, 2 ), &wsa_data );
 
-    SOCKET server;
-    SOCKET client;
     server = socket(AF_INET, SOCK_STREAM, 0);
-
-    int len;
-
-    SOCKADDR_IN server_addr;
-    SOCKADDR_IN client_addr;
 
     memset( &server_addr, 0, sizeof( server_addr ) );
     server_addr.sin_family = AF_INET;
@@ -89,13 +102,22 @@ int server( const char* ip )
     server_addr.sin_port = htons( 1234 );
     bind( server, (SOCKADDR*) &server_addr, sizeof( SOCKADDR ) );
 
+    // connect
     listen( server, 10 );
     cout << YELLOW << "waiting for response ..." << RESET << endl;
-
     len = sizeof( SOCKADDR );
     client = accept( server, ( SOCKADDR* ) &client_addr, &len );
     cout << GREEN <<  "[DONE]" << RESET << "  connected" << endl;
 
+    // contact
+    thread_send = thread( Send, client, send_buf );
+    thread_recv = thread( Recv, client, recv_buf );
+    thread_send.join();
+    thread_recv.join();
+
+    // disconnect
+    closesocket(server);
+    closesocket(client);
     WSACleanup();
 
     return 0;
@@ -116,9 +138,37 @@ int client( const char* ip )
     client_addr.sin_port = htons( 1234 );
     connect( client, (SOCKADDR*) &client_addr, sizeof( SOCKADDR ) );
 
+    // TODO 该写client的线程和收发函数了
     WSACleanup();
 
     return 0;
+}
+
+
+void Send( SOCKET client, char send_buf[] )
+{
+    signal( SIGINT, sig_handler );
+
+    while( keepRunning )
+    {
+        cin >> send_buf;
+        send( client, send_buf, 100, 0 );
+    }
+
+    cout << RED << " #$ --- disconnect --- $#" << RESET << endl;
+}
+
+void Recv( SOCKET client, char recv_buf[] )
+{
+    signal( SIGINT, sig_handler );
+
+    while( keepRunning )
+    {
+        recv( client, recv_buf, 100, 0 );
+        cout << recv_buf << endl;
+    }
+
+    cout << RED << " #$ --- disconnect --- $#" << RESET << endl;
 }
 
 void about()
@@ -134,10 +184,22 @@ void about()
     cout << RESET << endl;
 
     // user guide
+    cout << YELLOW << "Usage" << RESET << endl;
+    cout << GREEN << "  linker <mode> <IP>" << RESET << endl;
+    cout << "\n  mode:" << endl;
+    cout << "    server  ------  start linker as server" << endl;
+    cout << "    client  ------  start linker as client" << endl;
+    cout << "  IP:" << endl;
+    cout << "    server mode: your IP" << endl;
+    cout << "    client mode: IP of server\n" << endl;
     
-    // error code
-    cout << GREEN << "Error Code" << RESET << endl;
+    // return code
+    cout << YELLOW << "Return Code" << RESET << endl;
     cout << "  -1  argument is not correct" << endl;
+
+    // return code
+    cout << YELLOW << "Notice" << RESET << endl;
+    cout << "  message over 100 words is not supported" << endl;
 
     cout << endl;
 }
